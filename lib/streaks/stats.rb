53cf6c903bc6343eee917c4f66f2951fbbe691fa
Tony <tony@angel.co>
2019-04-20 17:41:40 -0400

    Inital commit

---
+++ lib/streaks/stats.rb
@@ -0,0 +1,8 @@
+require "streaks/stats/version"
+
+module Streaks
+  module Stats
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/streaks/stats/version.rb
@@ -0,0 +1,5 @@
+module Streaks
+  module Stats
+    VERSION = "0.1.0"
+  end
+end
